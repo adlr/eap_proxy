85ab8cb6e6dc158ebb7ee9930055717993f5a250
Andrew de los Reyes <adlr@chromium.org>
2018-06-01 21:47:57 -0700

    more improvements

--- eap_proxy.c
+++ eap_proxy.c
@@ -1,9 +1,14 @@
+#include <arpa/inet.h>
 #include <errno.h>
 #include <net/ethernet.h>
 #include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 /*
  * Idea: capture auth packets and forward
@@ -25,6 +30,8 @@
     }						\
   } while(0);
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 void usage(const char* me) {
   printf("usage: %s ont_iface_num int_iface_num\n", me);
   printf("  e.g. %s eth0 eth1\n", me);
@@ -38,8 +45,8 @@ int open_iface(const char* ethname) {
   struct ifreq ifr;
   memset(&ifr, 0, sizeof(ifr));
   strncpy(ifr.ifr_name, ethname, sizeof(ifr.ifr_name));
-  if ((ioctl(fd , SIOCGIFINDEX , &ifr)) == -1)
-  int index = 0;
+  if ((ioctl(fd , SIOCGIFINDEX , &ifr)) == -1) {}
+  //int index = 0;
   
 
   // Set promiscuous mode:
@@ -52,6 +59,30 @@ int open_iface(const char* ethname) {
   return fd;
 }
 
+void list_interfaces() {
+  printf("listing:\n");
+  for (int idx = 1; idx < 20; idx++) {
+    int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (fd < 0) {
+      perror("socket");
+      return;
+    }
+    struct ifreq ifr;
+    ifr.ifr_ifindex = idx;
+    int success = ioctl(fd, SIOCGIFNAME, &ifr) >= 0;
+    int rc;
+    do {
+      rc = close(fd);
+    } while (rc < 0 && errno == EINTR);
+    if (!success) {
+      printf("couldn't get name\n");
+    }
+    char name[IF_NAMESIZE + 1] = {0};
+    strncpy(name, ifr.ifr_name, IF_NAMESIZE);
+    printf("%s\n", name);
+  }
+}
+
 void move_packet(int from, int to) {
   unsigned char buf[2048];
   int rc = recv(from, buf, sizeof(buf), 0);
@@ -62,11 +93,16 @@ void move_packet(int from, int to) {
 }
 
 int main(int argc, char** argv) {
+  if (argc == 2 && !strcmp(argv[1], "-l")) {
+    list_interfaces();
+    return 0;
+  }
+
   if (argc < 3)
-    usage();
+    usage(argv[0]);
   int s_ont = open_iface(argv[1]);
   int s_int = open_iface(argv[2]);
-  int max_fd = max(s_ont, s_int);
+  int max_fd = MAX(s_ont, s_int);
 
   // route the packets
   fd_set read_fds;
